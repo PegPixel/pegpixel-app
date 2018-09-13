c9118dc6f5f41a1db758acc52cf105598f796e02
Michael Dörsam <m.doersam@mailbox.org>
2018-09-13 15:35:10 +0200

    Create Interface for different stages of BluetoothConnection

--- app/src/main/java/org/md/pegpixel/BluetoothConnectionToBoard.kt
+++ app/src/main/java/org/md/pegpixel/BluetoothConnectionToBoard.kt
@@ -1,52 +1,82 @@
 package org.md.pegpixel
 
-import android.app.Activity
 import android.bluetooth.BluetoothAdapter
-import android.content.Intent
-import android.support.v4.app.ActivityCompat.startActivityForResult
 import android.util.Log
 import android.bluetooth.BluetoothDevice
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.IntentFilter
-import java.nio.file.Files.size
+import android.bluetooth.BluetoothSocket
+import android.widget.Toast
+import java.io.IOException
 import java.util.*
 
-
-class BluetoothConnectionToBoard (private val bluetoothDevice: BluetoothDevice) {
-
-    private val theOneAndOnlyUuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-
-    fun sendData(data: String) {
-        val bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(theOneAndOnlyUuid)
-        bluetoothSocket.connect()
-
-        val outputStream = bluetoothSocket.outputStream
-
-        outputStream.write(data.toByteArray())
-
-        Log.i("STUFF", "Bluetooth socket to device is etablished")
-    }
-
+interface BluetoothConnectionToBoard {
+    fun sendData(data: String, handleConnectionError: (String) -> Unit)
+    fun close()
     companion object {
-        fun initiate(bluetoothDeviceName: String): BluetoothConnectionToBoard?{
+        fun initiate(bluetoothDeviceName: String): BluetoothConnectionToBoard{
             val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
             val pairedDevices = mBluetoothAdapter.bondedDevices
-            val pairedBluetoothDevices = pairedDevices.map {
-                PairedBluetoothDevice(it.name, it.address)
+            val pairedBluetoothDevices = pairedDevices.map { bluetoothDevice ->
+                PairedBluetoothDevice(bluetoothDevice.name, bluetoothDevice.address)
             }
 
             Log.i("STUFF", "paired devices: $pairedBluetoothDevices")
 
-            val macAdress = pairedDevices.find { it.name == bluetoothDeviceName }
+            val macAddress = pairedDevices.find { it.name == bluetoothDeviceName }
 
-            return macAdress?.let {
-                val bluetoothDevice = mBluetoothAdapter.getRemoteDevice(it.address)
-                BluetoothConnectionToBoard(bluetoothDevice)
+
+            return if(macAddress != null) {
+                val bluetoothDevice = macAddress.let { bluetoothDevice ->
+                    mBluetoothAdapter.getRemoteDevice(bluetoothDevice.address)
+                }
+                EstablishedBluetoothConnectionToBoard(bluetoothDevice)
+            } else {
+                PendingBluetoothConnectionToBoard(bluetoothDeviceName)
             }
         }
     }
 }
+
+class EstablishedBluetoothConnectionToBoard (private val bluetoothDevice: BluetoothDevice) : BluetoothConnectionToBoard {
+
+    private val theOneAndOnlyUuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    private val bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(theOneAndOnlyUuid)
+
+    override fun sendData(data: String, handleConnectionError: (String) -> Unit) {
+        bluetoothSocket.use { bluetoothSocket ->
+            try{
+                bluetoothSocket.connect()
+                writeToStream(bluetoothSocket, data)
+            } catch (e: IOException) {
+                handleConnectionError("Could not connect to ${bluetoothDevice.name}")
+            }
+        }
+    }
+
+    private fun writeToStream(bluetoothSocket: BluetoothSocket, data: String) {
+        bluetoothSocket.outputStream.use { outputStream ->
+            outputStream.write(data.toByteArray())
+        }
+    }
+    override fun close() {
+        bluetoothSocket.close()
+    }
+
+}
+class PendingBluetoothConnectionToBoard (private val bluetoothDeviceName: String) : BluetoothConnectionToBoard {
+
+    override fun sendData(data: String, handleConnectionError: (String) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun close() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class BluetoothConnectionSocket {
+
+}
+
 
 data class PairedBluetoothDevice(val name: String, val macAddress: String)
 
--- app/src/main/java/org/md/pegpixel/BoardView.kt
+++ app/src/main/java/org/md/pegpixel/BoardView.kt
@@ -4,25 +4,32 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TableLayout
+import android.widget.Toast
 
 
 class BoardView : AppCompatActivity() {
+
+    private val bluetoothDeviceName = "DSD TECH HC-05"
+    private var bluetoothConnectionToBoard: BluetoothConnectionToBoard =  PendingBluetoothConnectionToBoard(bluetoothDeviceName)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_view)
 
-        //val pegGrid = PegGrid(7, 5)
+        bluetoothConnectionToBoard = BluetoothConnectionToBoard.initiate(bluetoothDeviceName)
+
         val rootTable = findViewById<TableLayout>(R.id.pegTableLayout)
 
         PegGrid.addGridTo(columnCount = 7, rowCount = 5, tableLayout = rootTable)
 
-        val bluetoothConnectionToBoard = BluetoothConnectionToBoard.initiate("DSD TECH HC-05")
-
-        if(bluetoothConnectionToBoard == null) {
-            Log.e("STUFF", "Could not connect to bluetooth device")
+        bluetoothConnectionToBoard.sendData("Board is connected") { errorMessage ->
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
         }
-        bluetoothConnectionToBoard?.sendData("Hello World")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        bluetoothConnectionToBoard.close()
     }
 }
 
